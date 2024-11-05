129b2507f6e466359f1d2ef896ac6e4db366b816
Mahammad Hajiyev <113542710+MahammadHajiyev2024@users.noreply.github.com>
2024-11-04 19:29:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -112,5 +112,17 @@ with ui.layout_columns():
         def plotly_histogram():  
             fig = px.histogram(penguins_df, x="body_mass_g", color="species", title="Penguin Body Mass by Species")
             return fig
-           
+
+# --------------------------------------------------------
+# Reactive calculations and effects
+# --------------------------------------------------------
+
+# Add a reactive calculation to filter the data
+# By decorating the function with @reactive, we can use the function to filter the data
+# The function will be called whenever an input functions used to generate that output changes.
+# Any output that depends on the reactive function (e.g., filtered_data()) will be updated when the data changes.
+
+@reactive.calc
+def filtered_data():
+    return penguins_df
 
